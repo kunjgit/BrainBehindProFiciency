5338436b8ed64c96520e32c61b59358cdc34d86e
kunjgit <patelkunj2021@gmail.com>
2024-06-06 09:34:37 +0530

    updated db

--- Proficiency/Program.cs
+++ Proficiency/Program.cs
@@ -3,6 +3,9 @@ using Proficiency.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+
 // Add services to the container.
 
 builder.Services.AddControllers();
