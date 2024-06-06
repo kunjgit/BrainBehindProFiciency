d9cd771b288c906c31c8662c394eb7d96f85a1dd
kunjgit <patelkunj2021@gmail.com>
2024-06-06 09:44:08 +0530

    updated azurevault

--- Proficiency/Program.cs
+++ Proficiency/Program.cs
@@ -1,5 +1,12 @@
+using Azure.Identity;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
 using Microsoft.EntityFrameworkCore;
 using Proficiency.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +14,6 @@ var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
 builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,12 +30,7 @@ builder.Services.AddCors(o => o.AddPolicy("OurPolicy",
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Connection")));
 var app = builder.Build();
 
-
-
-
 app.UseCors("OurPolicy");
-
-
 
 if (app.Environment.IsDevelopment())
 {
